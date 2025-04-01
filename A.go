9465cf38706a43b41a34142acef02a82fb2d5f9b
jj1guj <jj1guj@jj1guj.net>
2025-04-01 13:21:01 +0000

    山登り法を実装
    スコアは悪化
    14621514

--- A.go
+++ A.go
@@ -2,152 +2,15 @@ package main
 
 import (
 	"bufio"
-	"container/heap"
 	"fmt"
 	"math"
+	"math/rand"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
-
-type Edge struct {
-	from   int
-	to     int
-	weight int
-}
-
-type EdgeHeap []Edge
-
-func (h EdgeHeap) Len() int           { return len(h) }
-func (h EdgeHeap) Less(i, j int) bool { return h[i].weight < h[j].weight }
-func (h EdgeHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *EdgeHeap) Push(x interface{}) {
-	*h = append(*h, x.(Edge))
-}
-
-func (h *EdgeHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
-// プリム法による最小全域木の構築
-func Prim(nodes *[]int, D *[][]int) [][]int {
-	// 隣接リストを構築
-	adj := make([][]Edge, len(*nodes))
-	for i := 0; i < len(*nodes); i++ {
-		for j := 0; j < len(*nodes); j++ {
-			if i != j {
-				adj[i] = append(adj[i], Edge{from: i, to: j, weight: (*D)[i][j]})
-			}
-		}
-	}
-
-	marked := make([]bool, len(*nodes))
-	for i := 0; i < len(*nodes); i++ {
-		marked[i] = false
-	}
-	marked_cnt := 0
-	heapq := &EdgeHeap{}
-	heap.Init(heapq)
-	marked[0] = true
-	marked_cnt++
-	for _, e := range adj[0] {
-		heap.Push(heapq, e)
-	}
-
-	out_adj := make([][]int, len(*nodes))
-	for marked_cnt < len(*nodes) {
-		e := heap.Pop(heapq).(Edge)
-		if marked[e.to] {
-			continue
-		}
-		marked[e.to] = true
-		marked_cnt++
-		out_adj[e.from] = append(out_adj[e.from], e.to)
-		out_adj[e.to] = append(out_adj[e.to], e.from)
-		for _, e2 := range adj[e.to] {
-			if !marked[e2.to] {
-				heap.Push(heapq, e2)
-			}
-		}
-	}
-	return out_adj
-}
-
-// トポロジカルソート
-func TopologicalSort(adj *[][]int, g *[]int) []int {
-	sorted_g := make([]int, 0)
-	indegrees := make([]int, len(*g))
-	for i := 0; i < len(*g); i++ {
-		for _, j := range (*adj)[i] {
-			indegrees[j]++
-		}
-	}
-
-	queue := make([]int, 0)
-	for i := 0; i < len(*g); i++ {
-		if indegrees[i] == 0 {
-			queue = append(queue, i)
-		}
-	}
-
-	cnt := 0
-	for len(queue) > 0 {
-		node := queue[0]
-		queue = queue[1:]
-		sorted_g = append(sorted_g, (*g)[node])
-		cnt++
-
-		for _, neighbor := range (*adj)[node] {
-			indegrees[neighbor]--
-			if indegrees[neighbor] == 0 {
-				queue = append(queue, neighbor)
-			}
-		}
-	}
-	return sorted_g
-}
-
-func dfs(adj *[][]int, g *[]int) []int {
-	sorted_g := make([]int, 0)
-	sorted_g_index := make([]int, 0)
-
-	visited := make([]bool, len(*g))
-	for i := 0; i < len(*g); i++ {
-		visited[i] = false
-	}
-
-	stack := make([]int, 0)
-	for i := 0; i < len(*g); i++ {
-		if len((*adj)[i]) <= 1 {
-			stack = append(stack, i)
-			break
-		}
-	}
-
-	for len(stack) > 0 {
-		node := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-		if visited[node] {
-			continue
-		}
-		visited[node] = true
-		sorted_g = append(sorted_g, (*g)[node])
-		sorted_g_index = append(sorted_g_index, node)
-
-		for _, neighbor := range (*adj)[node] {
-			if !visited[neighbor] {
-				stack = append(stack, neighbor)
-			}
-		}
-	}
-	return sorted_g
-}
 
 func toBlankJoin(arr *[]int) string {
 	strArr := make([]string, len(*arr))
@@ -177,6 +40,51 @@ func distSquared(a *[]int, b *[]int) int {
 	return ((*a)[0]-(*b)[0])*((*a)[0]-(*b)[0]) + ((*a)[1]-(*b)[1])*((*a)[1]-(*b)[1])
 }
 
+func sort_group(group *[]int, D *[][]int, d_sum *int) []int {
+	g := len(*group)
+	ret_group := make([]int, g)
+	first_city_id := -1
+	second_city_id := -1
+	d_min := math.MaxInt
+	marked := map[int]bool{}
+	for _, i := range *group {
+		marked[i] = false
+		for _, j := range *group {
+			if i != j {
+				if (*D)[i][j] < d_min {
+					d_min = (*D)[i][j]
+					first_city_id = i
+					second_city_id = j
+				}
+			}
+		}
+	}
+
+	ret_group[0] = first_city_id
+	ret_group[1] = second_city_id
+	(*d_sum) += d_min
+	marked[first_city_id] = true
+	marked[second_city_id] = true
+	for i := 2; i < g; i++ {
+		d_min = math.MaxInt
+		city_id := -1
+		for j := 0; j < i; j++ {
+			for _, id := range *group {
+				if !marked[id] {
+					if (*D)[ret_group[j]][id] < d_min {
+						d_min = (*D)[ret_group[j]][id]
+						city_id = id
+					}
+				}
+			}
+		}
+		ret_group[i] = city_id
+		marked[city_id] = true
+		(*d_sum) += d_min
+	}
+	return ret_group
+}
+
 func answer(groups *[][]int, edges *[][][]int, writer *bufio.Writer) {
 	writer.WriteString("!\n")
 	for i := 0; i < len(*groups); i++ {
@@ -189,6 +97,7 @@ func answer(groups *[][]int, edges *[][][]int, writer *bufio.Writer) {
 }
 
 func main() {
+	start := time.Now()
 	scanner := bufio.NewScanner(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 
@@ -238,25 +147,6 @@ func main() {
 			if i == j {
 				D[i][j] = 0
 			} else if i < j {
-				// coord_i := [][]int{
-				// 	{C[i][0], C[i][2]},
-				// 	{C[i][0], C[i][3]},
-				// 	{C[i][1], C[i][2]},
-				// 	{C[i][1], C[i][3]},
-				// }
-				// coord_j := [][]int{
-				// 	{C[j][0], C[j][2]},
-				// 	{C[j][0], C[j][3]},
-				// 	{C[j][1], C[j][2]},
-				// 	{C[j][1], C[j][3]},
-				// }
-				// d_max := 0
-				// for ci := range coord_i {
-				// 	for cj := range coord_j {
-				// 		d_max = max(d_max, distSquared(&coord_i[ci], &coord_j[cj]))
-				// 	}
-				// }
-				// D[i][j] = d_max
 				D[i][j] = distSquared(&C_coord[i], &C_coord[j])
 			} else {
 				D[i][j] = D[j][i]
@@ -298,12 +188,17 @@ func main() {
 	}
 	groups := make([][]int, M)
 
+	// 考えられるコストを記録する
+	d_sum := 0
+	d_sum_list := make([]int, M)
+
 	for _, g_id := range G_ids {
 		g := G[g_id]
 		slice := make([]int, g)
 		// 最初の都市を選ぶ
 		// 最初の都市は現状選べる最も近い都市との距離が最小である都市を選ぶ
 		first_city_id := -1
+		second_city_id := -1
 		first_d_min := math.MaxInt
 		ref := -1
 		for i := 0; i < N; i++ {
@@ -316,6 +211,7 @@ func main() {
 						if D[i][id] < first_d_min {
 							first_d_min = D[i][id]
 							first_city_id = i
+							second_city_id = id
 							break
 						}
 					}
@@ -324,10 +220,22 @@ func main() {
 		}
 		if first_city_id == -1 {
 			first_city_id = ref
+			d_sum_list[g_id] = 0
+			slice[0] = first_city_id
+			C_selected[first_city_id] = true
+		} else {
+			slice[0] = first_city_id
+			d_sum_list[g_id] = 0
+			C_selected[first_city_id] = true
+			if g > 1 {
+				d_sum_list[g_id] += first_d_min
+				slice[1] = second_city_id
+				C_selected[second_city_id] = true
+				d_sum += first_d_min
+			}
 		}
-		slice[0] = first_city_id
-		C_selected[first_city_id] = true
-		for idx := 1; idx < g; idx++ {
+
+		for idx := 2; idx < g; idx++ {
 			d_min := math.MaxInt
 			city_id := -1
 			for j := 0; j < idx; j++ {
@@ -340,17 +248,64 @@ func main() {
 					}
 				}
 			}
+			d_sum_list[g_id] += d_min
 			slice[idx] = city_id
+			d_sum += d_min
 			C_selected[city_id] = true
 		}
 		groups[g_id] = slice
 	}
 
-	// 最小全域木を構築し、トポロジカルソートを行う
-	for i := 0; i < M; i++ {
-		adj := Prim(&groups[i], &D)
-		sorted_g := dfs(&adj, &groups[i])
-		groups[i] = sorted_g
+	if M > 1 {
+		cnt := 0
+		for time.Since(start) <= 1500*time.Millisecond {
+			cnt += 1
+			g1 := rand.Intn(M)
+			g2 := rand.Intn(M)
+			for g1 == g2 {
+				g2 = rand.Intn(M)
+			}
+			g1_idx := rand.Intn(G[g1])
+			g2_idx := rand.Intn(G[g2])
+
+			tmp := groups[g1][g1_idx]
+			groups[g1][g1_idx] = groups[g2][g2_idx]
+			groups[g2][g2_idx] = tmp
+
+			rec := d_sum
+			var g1_new, g2_new []int
+			var rec_g1, rec_g2 int
+			if G[g1] > 1 {
+				rec -= d_sum_list[g1]
+				rec_g1 = 0
+				g1_new = sort_group(&groups[g1], &D, &rec_g1)
+				rec += rec_g1
+			}
+
+			if G[g2] > 1 {
+				rec -= d_sum_list[g2]
+				rec_g2 = 0
+				g2_new = sort_group(&groups[g2], &D, &rec_g2)
+				rec += rec_g2
+			}
+
+			if rec < d_sum {
+				d_sum = rec
+				if G[g1] > 1 {
+					groups[g1] = g1_new
+					d_sum_list[g1] = rec_g1
+				}
+
+				if G[g2] > 1 {
+					groups[g2] = g2_new
+					d_sum_list[g2] = rec_g2
+				}
+			} else {
+				tmp := groups[g1][g1_idx]
+				groups[g1][g1_idx] = groups[g2][g2_idx]
+				groups[g2][g2_idx] = tmp
+			}
+		}
 	}
 
 	edges := [][][]int{}
